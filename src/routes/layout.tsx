fda98d7b10d71e3902c78cd912d2b7911fe33cf4
a145789 <2091927351@qq.com>
2023-04-11 17:59:18 +0800

    chore: test

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from '@modern-js/runtime/router';
-import useIsSsr from '@/hooks/useIsSsr';
+// import useIsSsr from '@/hooks/useIsSsr';
 
 export default function Layout() {
-  const isSsr = useIsSsr();
-  if (isSsr) {
-    return null;
-  }
+  // const isSsr = useIsSsr();
+  // if (isSsr) {
+  //   return null;
+  // }
   return <Outlet />;
 }
