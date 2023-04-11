c084e4ee2046bb07b4358cdc262937083318909a
a145789 <2091927351@qq.com>
2023-04-11 16:51:51 +0800

    fix: ssr

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,10 @@
 import { Outlet } from '@modern-js/runtime/router';
+import useIsSsr from '@/hooks/useIsSsr';
 
 export default function Layout() {
+  const isSsr = useIsSsr();
+  if (isSsr) {
+    return null;
+  }
   return <Outlet />;
 }
